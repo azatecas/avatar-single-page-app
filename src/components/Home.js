4d94b97d14f9ab609d6d69764ff27da9a1540619
Luis PM <luisp237@gmail.com>
2020-04-08 03:34:00 -0400

    initialized episodes data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Particles from 'react-particles-js';
+import { particleAttrs } from './particlesData';
 import { findByLabelText } from '@testing-library/react';
 
 const ParentDiv = styled.div`
@@ -15,133 +16,11 @@ const MyDiv = styled.div`
   position: absolute;
 ` //MyDiv
 
-const Appa = styled.img`
-  width: 25%;
-  position: relative;
-
-  @media (max-width: 768px) {
-    width: 50%;
-  }
-
-  @media (max-width: 600px) {
-    width: 70%;
-  }
-` //Appa
 
 const SecDiv = styled.div`    
    z-index: 10;
 ` //SecDiv
 
-const particleAttrs = {
-    "particles": {
-      "number": {
-        "value": 50,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "star",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.7210787337857428,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 4,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 100,
-          "size_min": 8.926325356444401,
-          "sync": true
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": .2,
-        "direction": "left",
-        "random": false,
-        "straight": true,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 200,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": false
-  }
 
 const Home = () => {
 
@@ -165,8 +44,13 @@ const Home = () => {
                   left:"0"}}/>
                  
               </div>
-
-              <Appa src={require('./appa.svg')} alt="appa"/>
+            <div className="appa-cont">
+              <img 
+                src={require('./appa.svg')} 
+                alt="appa"
+                className="appa-flying"
+                />
+            </div>
                <div style={{
                  position:"relative",
                  fontSize:"2rem",
---
+++ src/components/episodeData.js
@@ -0,0 +1,558 @@
+export const avatarEpisodes = {
+    "book1":[
+        {
+            "id":1,
+            "title":"The Boy in the Iceberg",
+            "director":"Dave Filoni",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"JM Animation",
+            "air_date":"February 21, 2005",
+            "prod_code":101
+        },
+        {
+            "id":2,
+            "title":"The Avatar Returns",
+            "director":"Dave Filoni",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"JM Animation",
+            "air_date":"February 21, 2005",
+            "prod_code":102
+        },
+        {
+            "id":3,
+            "title":"The Southern Air Temple",
+            "director":"Lauren MacMullan",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"DR Movie",
+            "air_date":"February 25, 2005",
+            "prod_code":103
+        },
+        {
+            "id":4,
+            "title":"The Warriors of Kyoshi",
+            "director":"Giancarlo Volpe",
+            "writter":["Nick Malis"],
+            "animator":"JM Animation",
+            "air_date":"March 4, 2005",
+            "prod_code":104
+        },
+        {
+            "id":5,
+            "title":"The King of Omashu",
+            "director":"Anthony Lioi",
+            "writter":["John O'Bryan"],
+            "animator":"DR Movie",
+            "air_date":"March 18, 2005",
+            "prod_code":105
+        },
+        {
+            "id":6,
+            "title":"Imprisoned",
+            "director":"Dave Filoni",
+            "writter":["Matthew Hubbard"],
+            "animator":"JM Animation",
+            "air_date":"March 25, 2005",
+            "prod_code":106
+        },
+        {
+            "id":7,
+            "title":"Winter Solstice Part 1",
+            "director":"Lauren MacMullan",
+            "writter":["Aaron Ehasz"],
+            "animator":"DR Movie",
+            "air_date":"April 8, 2005",
+            "prod_code":107
+        },
+        {
+            "id":8,
+            "title":"Winter Solstice Part 2",
+            "director":"Giancarlo Volpe",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"DR Movie",
+            "air_date":"April 15, 2005",
+            "prod_code":108
+        },
+        {
+            "id":9,
+            "title":"The Waterbending Scroll",
+            "director":"Anthony Lioi",
+            "writter":["John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"April 29, 2005",
+            "prod_code":109
+        },
+        {
+            "id":10,
+            "title":"Jet",
+            "director":"Dave Filoni",
+            "writter":["James Eagan"],
+            "animator":"JM Animation",
+            "air_date":"May 6, 2005",
+            "prod_code":110
+        },
+        {
+            "id":11,
+            "title":"The Great Divide",
+            "director":"Giancarlo Volpe",
+            "writter":["John O'Bryan"],
+            "animator":"DR Movie",
+            "air_date":"May 20, 2005",
+            "prod_code":111
+        },
+        {
+            "id":12,
+            "title":"The Storm",
+            "director":"Lauren MacMullan",
+            "writter":["Aaron Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"June 3, 2005",
+            "prod_code":112
+        },
+        {
+            "id":13,
+            "title":"The Blue Spirit",
+            "director":"Dave Filoni",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"DR Movie",
+            "air_date":"June 17, 2005",
+            "prod_code":113
+        },
+        {
+            "id":14,
+            "title":"The Fortuneteller",
+            "director":"Dave Filoni",
+            "writter":["Aaron Ehasz","John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"September 23, 2005",
+            "prod_code":114
+        },
+        {
+            "id":15,
+            "title":"Bato of the Water Tribe",
+            "director":"Giancarlo Volpe",
+            "writter":["Ian Wilcox"],
+            "animator":"DR Movie",
+            "air_date":"October 7, 2005",
+            "prod_code":115
+        },
+        {
+            "id":16,
+            "title":"The Deserter",
+            "director":"Lauren MacMullan",
+            "writter":["Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"October 21, 2005",
+            "prod_code":116
+        },
+        {
+            "id":17,
+            "title":"The Northern Air Temple",
+            "director":"Dave Filoni",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"DR Movie",
+            "air_date":"November 4, 2005",
+            "prod_code":117
+        },
+        {
+            "id":18,
+            "title":"The Waterbending Master",
+            "director":"Giancarlo Volpe",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"JM Animation",
+            "air_date":"November 18, 2005",
+            "prod_code":118
+        },
+        {
+            "id":19,
+            "title":"The Siege of the North Part 1",
+            "director":"Lauren MacMullan",
+            "writter":["John O'Bryan"],
+            "animator":"DR Movie",
+            "air_date":"December 2, 2005",
+            "prod_code":119
+        },
+        {
+            "id":20,
+            "title":"The Siege of the North Part 2",
+            "director":"Dave Filoni",
+            "writter":["Aaron Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"December 2, 2005",
+            "prod_code":120
+        },
+    ],
+    "book2":[
+        {
+            "id":21,
+            "title":"The Avatar State",
+            "director":"Giancarlo Volpe",
+            "writter":["Aaron Ehasz","Elizabeth Welch Ehasz","Tim Hedrick","John O'Bryan"],
+            "animator":"DR Movie",
+            "air_date":"March 17, 2006",
+            "prod_code":201
+        },
+        {
+            "id":22,
+            "title":"The Cave of Two Lovers",
+            "director":"Lauren MacMullan",
+            "writter":["Joshua Hamilton"],
+            "animator":"JM Animation",
+            "air_date":"March 24, 2006",
+            "prod_code":202
+        },
+        {
+            "id":23,
+            "title":"Return to Omashu",
+            "director":"Ethan Spaulding",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"DR Movie",
+            "air_date":"April 7, 2006",
+            "prod_code":203
+        },
+        {
+            "id":24,
+            "title":"The Swamp",
+            "director":"Giancarlo Volpe",
+            "writter":["Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"April 14, 2006",
+            "prod_code":204
+        },
+        {
+            "id":25,
+            "title":"Avatar Day",
+            "director":"Lauren MacMullan",
+            "writter":["John O'Bryan"],
+            "animator":"DR Movie",
+            "air_date":"April 28, 2006",
+            "prod_code":205
+        },
+        {
+            "id":26,
+            "title":"The Blind Bandit",
+            "director":"Ethan Spaulding",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"JM Animation",
+            "air_date":"May 5, 2006",
+            "prod_code":206
+        },
+        {
+            "id":27,
+            "title":"Zuko Alone",
+            "director":"Lauren MacMullan",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"May 12, 2006",
+            "prod_code":207
+        },
+        {
+            "id":28,
+            "title":"The Chase",
+            "director":"Giancarlo Volpe",
+            "writter":["Joshua Hamilton"],
+            "animator":"DR Movie",
+            "air_date":"May 26, 2006",
+            "prod_code":208
+        },
+        {
+            "id":29,
+            "title":"Bitter Work",
+            "director":"Ethan Spaulding",
+            "writter":["Aaron Ehasz"],
+            "animator":"DR Movie",
+            "air_date":"June 2, 2006",
+            "prod_code":209
+        },
+        {
+            "id":30,
+            "title":"The Library",
+            "director":"Giancarlo Volpe",
+            "writter":["John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"July 14, 2006",
+            "prod_code":210
+        },
+        {
+            "id":31,
+            "title":"The Desert",
+            "director":"Lauren MacMullan",
+            "writter":["Tim Hedrick"],
+            "animator":"DR Movie",
+            "air_date":"July 14, 2006",
+            "prod_code":211
+        },
+        {
+            "id":32,
+            "title":"The Serpent's Pass",
+            "director":"Ethan Spaulding",
+            "writter":["Michael Dante DiMartino","Joshua Hamilton"],
+            "animator":"JM Animation",
+            "air_date":"September 15, 2006",
+            "prod_code":212
+        },
+        {
+            "id":33,
+            "title":"The Drill",
+            "director":"Giancarlo Volpe",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"DR Movie",
+            "air_date":"September 15, 2006",
+            "prod_code":213
+        },
+        {
+            "id":34,
+            "title":"City of Walls and Secrets",
+            "director":"Lauren MacMullan",
+            "writter":["Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"September 22, 2006",
+            "prod_code":214
+        },
+        {
+            "id":35,
+            "title":"The Tales of Ba Sing Se",
+            "director":"Ethan Spaulding",
+            "writter":["Joann Estoesta","Lisa Wahlander","Andrew Huebner","Gary Scheppke","Lauren MacMullan","Katie Mattila","Justin Ridge","Giancarlo Volpe"],
+            "animator":"DR Movie",
+            "air_date":"September 29, 2006",
+            "prod_code":215
+        },
+        {
+            "id":36,
+            "title":"Appa's Lost Days",
+            "director":"Giancarlo Volpe",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"October 13, 2006",
+            "prod_code":216
+        },
+        {
+            "id":37,
+            "title":"Lake Laogai",
+            "director":"Lauren MacMullan",
+            "writter":["Tim Hedrick"],
+            "animator":"DR Movie",
+            "air_date":"November 3, 2006",
+            "prod_code":217
+        },
+        {
+            "id":38,
+            "title":"The Earth King",
+            "director":"Ethan Spaulding",
+            "writter":["John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"November 17, 2006",
+            "prod_code":218
+        },
+        {
+            "id":39,
+            "title":"The Guru",
+            "director":"Giancarlo Volpe",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"DR Movie",
+            "air_date":"December 1, 2006",
+            "prod_code":219
+        },
+        {
+            "id":40,
+            "title":"The Crossroads of Destiny",
+            "director":"Michael Dante DiMartino",
+            "writter":["Aaron Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"December 1, 2006",
+            "prod_code":220
+        },
+
+    ],
+    "book3":[
+        {
+            "id":41,
+            "title":"The Awakening",
+            "director":"Giancarlo Volpe",
+            "writter":["Aaron Ehasz"],
+            "animator":"Moi Animation",
+            "air_date":"September 21, 2007",
+            "prod_code":301
+        },
+        {
+            "id":42,
+            "title":"The Headband",
+            "director":"Joaquim Dos Santos",
+            "writter":["John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"September 22, 2007",
+            "prod_code":302
+        },
+        {
+            "id":43,
+            "title":"The Painted Lady",
+            "director":"Ethan Spaulding",
+            "writter":["Joshua Hamilton"],
+            "animator":"Moi Animation",
+            "air_date":"October 5, 2007",
+            "prod_code":303
+        },
+        {
+            "id":44,
+            "title":"Sokka's Master",
+            "director":"Giancarlo Volpe",
+            "writter":["Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"October 12, 2007",
+            "prod_code":304
+        },
+        {
+            "id":45,
+            "title":"The Beach",
+            "director":"Joaquim Dos Santos",
+            "writter":["Katie Mattila"],
+            "animator":"Moi Animation",
+            "air_date":"October 19, 2007",
+            "prod_code":305
+        },
+        {
+            "id":46,
+            "title":"The Avatar and the Fire Lord",
+            "director":"Ethan Spaulding",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"JM Animation",
+            "air_date":"October 26, 2007",
+            "prod_code":306
+        },
+        {
+            "id":47,
+            "title":"The Runaway",
+            "director":"Giancarlo Volpe",
+            "writter":["Joshua Hamilton"],
+            "animator":"Moi Animation",
+            "air_date":"November 2, 2007",
+            "prod_code":307
+        },
+        {
+            "id":48,
+            "title":"The Puppetmaster",
+            "director":"Joaquim Dos Santos",
+            "writter":["Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"November 9, 2007",
+            "prod_code":308
+        },
+        {
+            "id":49,
+            "title":"Nightmares and Daydreams",
+            "director":"Ethan Spaulding",
+            "writter":["John O'Bryan"],
+            "animator":"Moi Animation",
+            "air_date":"November 16, 2007",
+            "prod_code":309
+        },
+        {
+            "id":50,
+            "title":"The Day of Black Sun Part 1",
+            "director":"Giancarlo Volpe",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"JM Animation",
+            "air_date":"November 30, 2007",
+            "prod_code":310
+        },
+        {
+            "id":51,
+            "title":"The Day of Black Sun Part 2",
+            "director":"Joaquim Dos Santos",
+            "writter":["Aaron Ehasz"],
+            "animator":"Moi Animation",
+            "air_date":"November 30, 2007",
+            "prod_code":311
+        },
+        {
+            "id":52,
+            "title":"The Western Air Temple",
+            "director":"Ethan Spaulding",
+            "writter":["Elizabeth Welch Ehasz","Tim Hedrick"],
+            "animator":"JM Animation",
+            "air_date":"July 14, 2008",
+            "prod_code":312
+        },
+        {
+            "id":53,
+            "title":"The Firebending Masters",
+            "director":"Giancarlo Volpe",
+            "writter":["John O'Bryan"],
+            "animator":"Moi Animation",
+            "air_date":"July 15, 2008",
+            "prod_code":313
+        },
+        {
+            "id":54,
+            "title":"The Boiling Rock Part 1",
+            "director":"Joaquim Dos Santos",
+            "writter":["May Chan"],
+            "animator":"JM Animation",
+            "air_date":"July 16, 2008",
+            "prod_code":314
+        },
+        {
+            "id":55,
+            "title":"The Boiling Rock Part 2",
+            "director":"Ethan Spaulding",
+            "writter":["Joshua Hamilton"],
+            "animator":"Moi Animation",
+            "air_date":"July 16, 2008",
+            "prod_code":315
+        },
+        {
+            "id":56,
+            "title":"The Southern Raiders",
+            "director":"Joaquim Dos Santos",
+            "writter":["Elizabeth Welch Ehasz"],
+            "animator":"Moi Animation",
+            "air_date":"July 17, 2008",
+            "prod_code":316
+        },
+        {
+            "id":57,
+            "title":"The Ember Island Players",
+            "director":"Giancarlo Volpe",
+            "writter":["Tim Hedrick","Josh Hamilton","John O'Bryan"],
+            "animator":"JM Animation",
+            "air_date":"July 18, 2008",
+            "prod_code":317
+        },
+        {
+            "id":58,
+            "title":"Sozin's Comet Part 1",
+            "director":"Ethan Spaulding",
+            "writter":["Michael Dante DiMartino"],
+            "animator":"JM Animation",
+            "air_date":"July 19, 2008",
+            "prod_code":318
+        },
+        {
+            "id":59,
+            "title":"Sozin's Comet Part 2",
+            "director":"Giancarlo Volpe",
+            "writter":["Aaron Ehasz"],
+            "animator":"Moi Animation",
+            "air_date":"July 19, 2008",
+            "prod_code":319
+        },
+        {
+            "id":60,
+            "title":"Sozin's Comet Part 3",
+            "director":"Joaquim Dos Santos",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"JM Animation",
+            "air_date":"July 19, 2008",
+            "prod_code":320
+        },
+        {
+            "id":61,
+            "title":"Sozin's Comet Part 4",
+            "director":"Joaquim Dos Santos",
+            "writter":["Michael Dante DiMartino","Bryan Konietzko"],
+            "animator":"JM Animation",
+            "air_date":"July 19, 2008",
+            "prod_code":321
+        },
+    ]
+}
